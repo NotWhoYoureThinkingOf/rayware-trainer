f0ee6d2bedcff17c16fb16eb29ffd1793bca23d7
NotWhoYoureThinkingOf <acurtis1286@gmail.com>
2021-06-24 20:23:46 -0400

    mac will close and reset. only needs symbols and will come back to that later.

--- src/components/MacModal.js
+++ src/components/MacModal.js
@@ -9,6 +9,8 @@ import { useDispatch, useSelector } from "react-redux";
 const MacModal = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   const [aboutOpen, setAboutOpen] = useState(false);
+  const [macStep1, setMacStep1] = useState(true);
+  const [macStep2, setMacStep2] = useState(false);
   const macMockup = useRef(null);
   const macAboutMenu = useSelector(selectMacAbout);
   const dispatch = useDispatch();
@@ -30,10 +32,21 @@ const MacModal = () => {
     }
   }, [menuOpen]);
 
+  useEffect(() => {
+    if (!macAboutMenu) {
+      setMacStep1(true);
+      setMacStep2(false);
+      setMenuOpen(false);
+      setAboutOpen(false);
+    }
+  }, [macAboutMenu]);
+
   const openAboutMenu = () => {
     setMenuOpen(false);
     setAboutOpen(true);
     dispatch(grabMacAbout());
+    setMacStep1(false);
+    setMacStep2(true);
   };
 
   return (
@@ -79,6 +92,35 @@ const MacModal = () => {
           <span>12:00 PM</span>
         </div>
       </div>
+      <div className={styles.macModal__steps}>
+        {macStep1 && (
+          <p className={styles.macModal__step}>
+            Click on the Apple icon in the top left and then click "About This
+            Mac"
+          </p>
+        )}
+        {macStep2 && (
+          <>
+            <p className={styles.macModal__step}>
+              This menu will list out the components on your Mac. One thing to
+              note is if your Mac has an M1 chip, you may not have a separate
+              graphics card as the M1 chip handles graphics as well.
+            </p>
+            <div className={styles.macModal__specs}>
+              <p>Minimum System Requirements:</p>
+              <p>Windows 10 64-bit / MacOS 10.15 Catalina</p>
+              <p>Intel i5 / Ryzen 7000 Series CPU</p>
+              <p>Nvidia GTX 1060 6GB / Radeon 560</p>
+              <p>16GB DDR4 RAM</p>
+            </div>
+            <p className={styles.macModal__step}>
+              If you need more specific hardware specs, click on "System
+              Report..." button.
+            </p>
+          </>
+        )}
+      </div>
+
       {macAboutMenu && <AboutThisMac />}
     </div>
   );
