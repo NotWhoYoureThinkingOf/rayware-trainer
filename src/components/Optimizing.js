764a4b72a98e6c41bdc09b8ffa6174ff49755237
NotWhoYoureThinkingOf <acurtis1286@gmail.com>
2021-07-15 10:35:01 -0400

    added some merkup to the optimizing screen. looking into implementing right click menu for mockup windows instructions on geforce control panel.

--- src/components/Optimizing.js
+++ src/components/Optimizing.js
@@ -5,5 +5,39 @@ import { Close, Search } from "@material-ui/icons";
 import gsap, { Power4 } from "gsap";
 
 export const Optimizing = () => {
-  return <div className={styles.optimizing}></div>;
+  const [windowsModal, setWindowsModal] = useState(false);
+
+  return (
+    <div className={styles.optimizing}>
+      <h2>Optimizing your graphics card:</h2>
+      <div className={styles.optimizing__info}>
+        <p>
+          If you've checked to ensure that your computer has a graphics card and
+          it meets the minimum system requirements, but RayWare still seems to
+          be running slow, you may just need to optimize your graphics card for
+          RayWare to fully utilize it.
+        </p>
+        <div className={styles.optimizing__OS}>
+          <h3>Click on your operating system to learn how to optimize:</h3>
+          <div className={styles.optimizing__systems}>
+            <div
+              className={styles.optimizing__windows}
+              onClick={() => setWindowsModal(true)}
+            >
+              <div className={styles.optimizing__windowsImage}>
+                <Image
+                  src="/windows-icon.jpg"
+                  layout="fill"
+                  objectFit="contain"
+                />
+              </div>
+
+              {/* <img src="/windows-icon.JPG" alt="" /> */}
+              <h3>Windows</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
